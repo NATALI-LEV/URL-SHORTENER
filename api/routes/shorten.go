4fcdef4cc833945946f70c6cfa1c7a8ebc5ad74d
NATALI-LEV <natalillev@gmail.com>
2025-02-25 14:16:59 +0200

    shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -15,3 +15,18 @@ type response struct {
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err := c.BodyParser(body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
+	}
+
+	//implement rate limiting
+	//check if the input is an actual url
+	//check for domain error
+	//enforce https, SSL
+
+}
