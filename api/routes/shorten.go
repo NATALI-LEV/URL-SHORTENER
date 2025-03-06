6e4b186b7c1e140107dbe602d2b913da9ced0be8
NATALI-LEV <natalillev@gmail.com>
2025-03-06 14:58:36 +0200

    shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -9,6 +9,7 @@ import (
 	"github.com/NATALI-LEV/URL-SHORTENER/api/helpers"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -67,6 +68,33 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "URL custom short is already in use",
+		})
+	}
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connnect to server",
+		})
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 }
